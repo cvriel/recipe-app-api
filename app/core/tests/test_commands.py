12e22763b87ca23eb0cf3f3d923c046c20e0c961
Chris van Riel <c.van.riel@amsterdam.nl>
2020-05-26 08:12:36 +0200

    testing

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 
 
 class CommandTests(TestCase):
+    
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
 
--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -45,7 +45,7 @@ class PrivateTagsApiTests(TestCase):
 
         tags = Tag.objects.all().order_by("-name")
         serializer = TagSerializer(tags, many=True)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)  # HTTP_200_OK
         self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
