d7f3a60622b99458fbbeb79fe48ac32dd0a2d646
Chris van Riel <c.van.riel@amsterdam.nl>
2020-06-01 11:37:23 +0200

    added upload feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -41,3 +41,11 @@ class RecipeDetailSerializer(RecipeSerializer):
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
 
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for image object"""
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "image")
+        read_only_fields = ("id",)
